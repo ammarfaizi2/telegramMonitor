bf7aa97c9a221210b3ae679a44931b7d712714fd
Ammar Faizi <ammarfaizi2@gmail.com>
2018-10-04 16:48:48 +0700

    Prepare channel info handler

---
+++ py/channel_info_handler.py
@@ -0,0 +1,31 @@
+from init import *
+
+import json
+
+realinput = process_stdin()
+json_input = json.loads(realinput)
+
+user = db.users_info.find_one({"user_id": json_input["user_id"]})
+if user == None:
+	print(db.users_info.insert({
+		"user_id": json_input["user_id"],
+		"info": json_input["info"],
+		"updated_at": None,
+		"updated_at_unix": None
+	}))
+else:
+	db.users_info.update(
+		{
+			"_id": user["_id"]
+		},
+		{
+			"user_id": json_input["user_id"],
+			"info": json_input["info"],
+			"updated_at": json_input["date"],
+			"updated_at_unix": json_input["unix_date"]
+		},
+		**{
+			"upsert": True
+		}
+	)
+	print(str(user["_id"]))
